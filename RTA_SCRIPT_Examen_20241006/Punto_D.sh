a88fd4fe7c7262d45c0b7753da97777c1b885c99
CristianDelgado31 <cristian-31-10@hotmail.com>
2024-10-08 01:13:46 +0000

    ADD: Punto D: (Estructuras Asimétrica)

--- RTA_SCRIPT_Examen_20241006/Punto_D.sh
+++ RTA_SCRIPT_Examen_20241006/Punto_D.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
+# Ruta al directorio home del usuario
+HOME_DIR="/home/vagrant"
+
 # Crear el directorio principal
-mkdir -p Estructura_Asimetrica/correo
-mkdir -p Estructura_Asimetrica/clientes
+mkdir -p "$HOME_DIR/Estructura_Asimetrica/correo"
+mkdir -p "$HOME_DIR/Estructura_Asimetrica/clientes"
 
 # Crear los archivos cartas_1 a cartas_100 en ambas carpetas
 for i in {1..100}; do
-  touch Estructura_Asimetrica/correo/cartas_$i
-  touch Estructura_Asimetrica/clientes/cartas_$i
+  touch "$HOME_DIR/Estructura_Asimetrica/correo/cartas_$i"
+  touch "$HOME_DIR/Estructura_Asimetrica/clientes/cartas_$i"
 done
 
 # Crear los archivos carteros_1 a carteros_10 en la carpeta correo
 for i in {1..10}; do
-  touch Estructura_Asimetrica/correo/carteros_$i
+  touch "$HOME_DIR/Estructura_Asimetrica/correo/carteros_$i"
 done
 
 # Comando de validación (mostrar la estructura de directorios)
-tree Estructura_Asimetrica/ --noreport | pr -T -s' ' -w 80 --column 4
+tree "$HOME_DIR/Estructura_Asimetrica/" --noreport | pr -T -s' ' -w 80 --column 4
 
 
